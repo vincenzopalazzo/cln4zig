3a9b4e9d28150311c52883fe6ef8e6b6e1fcc132
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-28 00:01:22 +0100

    ci: configure cln
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- docker/sandbox/run-bitcoin.sh
+++ docker/sandbox/run-bitcoin.sh
@@ -2,4 +2,4 @@
 DIR=/workdir
 bitcoind -datadir=$DIR/bitcoin_dir -server -regtest --daemon
 ps aux | grep bitcoind
-bitcoin-cli -datadir=$DIR/bitcoin_dir -rpcwait createwallet "go"
+bitcoin-cli -datadir=$DIR/bitcoin_dir -rpcwait createwallet "zig"
